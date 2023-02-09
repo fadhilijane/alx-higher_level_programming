c209513ed5a3ee25e38882350d8ae5665941e20b
Jane Fadhili <janefadhili@gmail.com>
2023-02-09 07:12:30 -0800

    data

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,8 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
 
- def best_score(a_dictionary):
-     if not a_dictionary:
-         return (None)
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return (None)
      else:
          new_dict = (max(a_dictionary, key=a_dictionary.get))
          return new_dict
