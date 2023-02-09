b659acf4df317e3a536231211e121fd3787c289b
Jane Fadhili <janefadhili@gmail.com>
2023-02-09 06:29:53 -0800

    dATA

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    sorted_dictionary = sorted(a_dictionary)
+    for i in sorted_dictionary:
+        print("{:s}:{}".format(i, a_dictionary[i]))
