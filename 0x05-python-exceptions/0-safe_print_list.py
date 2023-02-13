9bc5a9f100eac3d788b4a3e5b1cc6cfea3a50935
Jane Fadhili <janefadhili@gmail.com>
2023-02-13 04:39:56 -0800

    data

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
     count = 0
 
@@ -7,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
-        except:
-            pass
+        except IndexError:
+            break
     print()
     return count
