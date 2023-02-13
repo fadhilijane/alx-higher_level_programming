7ba2583430e7f88d5194203697c2e533986910bd
Jane Fadhili Njaga <111033901+fadhilijane@users.noreply.github.com>
2023-02-13 15:20:22 +0300

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,2 +1,12 @@
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
+    count = 0
+
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except:
+            pass
+    print()
+    return count
