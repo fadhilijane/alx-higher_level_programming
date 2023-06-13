d739869aeaa4cc735c4be59b310d1a9404fecb57
Jane Fadhili Njaga <janefadhili@gmail.com>
2023-06-13 08:11:04 -0700

    write in a file"

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" module: 1-write_file
+writes a string to a txt file
+"""
+
+
+def write_file(filename="", text=""):
+    """Writes text in a newfile
+
+    Args:
+    filename: new file created
+    text: text written
+    """
+    with open(filename, mode="w", encoding="utf-8") as myFile:
+        myFile.write(text)
+        print(len(text), end="")
