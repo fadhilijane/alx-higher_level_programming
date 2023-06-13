390988292220b297c81925790ff9512db6814cb8
Jane Fadhili Njaga <janefadhili@gmail.com>
2023-06-13 07:44:24 -0700

    adding files

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,10 +2,14 @@
 """module: 0-read_file
 the function reads the text file provided
 """
+
+
 def read_file(filename=""):
+
     """ Reads the txt file and shows in the stdout
     Args:
     -filename: file to be read
     """
+
     with open(filename, encoding="utf-8") as theFile:
         print(theFile.read())
