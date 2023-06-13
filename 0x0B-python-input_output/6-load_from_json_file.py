4ca8d737bf76930faeb3b63caffc6f7b86869dc0
Jane Fadhili Njaga <janefadhili@gmail.com>
2023-06-13 12:32:08 -0700

    from json file to an obj

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module: 6-load_from_json_file
+The function creates an object from a json file
+"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """create an object from json file
+
+    Args:
+    filename: the json file to be converted
+    """
+    with open(filename, 'w') as my_file:
+        return json.load(filename)
