5ff9f3a7978c5eeb31842c03469834310b76abe5
Jane Fadhili Njaga <janefadhili@gmail.com>
2023-06-13 08:15:47 -0700

    write in a file"

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -13,4 +13,5 @@ def write_file(filename="", text=""):
     """
     with open(filename, mode="w", encoding="utf-8") as myFile:
         myFile.write(text)
-        return (len(text), end="")
+        text_length = len(text)
+        return (text_length, end="")
