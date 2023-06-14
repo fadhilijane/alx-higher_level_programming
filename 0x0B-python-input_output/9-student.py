cf893a6fcc5dc155b845991466d391ef4cab1189
Jane Fadhili Njaga <janefadhili@gmail.com>
2023-06-13 19:53:20 -0700

    json in class and object

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Module: 9-student
+creates a class that defines student
+"""
+
+
+import json
+
+
+class Student:
+    """
+    gives public xtics of students
+
+    -first_name
+    -last_name
+    -age
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """ object instantion of student
+
+        Args:
+            first_name
+            last_name
+            age
+            """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ retrieves the dictionary representation of student
+
+        Args:
+        self"""
+
+        return self.__dict__
