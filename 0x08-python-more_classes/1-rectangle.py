f21306c2a76c607bd73978f682b132d149ebafb4
Jane Fadhili Njaga <janefadhili@gmail.com>
2023-06-15 05:20:20 -0700

    pycodestyle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,20 +8,20 @@ class Rectangle:
     """Rectangle class is defined by width and height."""
 
     def __init__(self, width=0, height=0):
-            """Initializes a Rectangle instance.
-            
-            Args:
-                width: width of rectangle
-                height: height of a rectangle
-            """
-            self.width = width
-            self.height = height
-    
+        """Initializes a Rectangle instance.
+
+        Args:
+            width: width of rectangle
+            height: height of a rectangle
+        """
+        self.width = width
+        self.height = height
+
     @property
     def width(self):
         """Retrieves the width of a Rectangle instance."""
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         """sets the width of the Rectangle instance
@@ -34,12 +34,12 @@ class Rectangle:
         if value < 0:
             raise ValueError("Width must be >= 0")
         self.__width = value
-    
+
     @property
     def height(self):
         """Gets the height of the Rectangle instance."""
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         """sets the height of the Rectangle instance
