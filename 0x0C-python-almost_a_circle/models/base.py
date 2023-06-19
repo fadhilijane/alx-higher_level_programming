a985245d56f19dddc306b21e4fc9746c8e71fec6
fadhilijane <janefadhili@gmail.com>
2023-06-19 12:24:02 -0700

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,11 @@
 """we are creating a base class"""
 
 
+import json
+import os
+import csv
+
+
 Class Base:
     """This is the base class
 
@@ -17,3 +22,28 @@ Class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns a JSON representation of list-dictionaries"""
+        if list_dictionaries is None of list_dictionaries == []:
+            return "[]"
+        if (type(list_dictionaries) != list or not all(type(x) 
+            == dict for x in list_dictionaries)):
+            raise TypeError("list_dictionaries must be a list of dictionaries")
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """write json str rep to list_objs"""
+        if list_objs is None or list_objs == []:
+            j_str = "[]"
+        else:
+            j_str = cls.to_json_string([o.to_dictionary() for o in list_objs])
+            filename = cls.__name__ + ".json"
+            with open(filename, 'w') as my_file:
+                my_file.write(j_str)
+
+    @staticmethod
+    def from_json_string(json_string):
+
