ed202d1e10cfad530af02d80a3769d2f905fad20
Jane Fadhili Njaga <janefadhili@gmail.com>
2023-07-18 16:13:57 -0700

    removing quotes from column m]name

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,10 @@
 -- create a second table full of data
 
-CREATE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+CREATE IF NOT EXISTS second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
 INSERT INTO second_table (id, name, score) VALUES
 (1, 'John', 10),
 (2, 'Alex', 3),
