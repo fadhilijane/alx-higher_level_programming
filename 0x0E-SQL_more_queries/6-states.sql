6415f8de14e1c77cf14d35adc7273b040cfa5999
Jane Fadhili Njaga <janefadhili@gmail.com>
2023-07-19 12:00:20 -0700

    6-states.sql

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,11 @@
+-- creates the database hbtn_0d_usa
+-- the table states (in the database hbtn_0d_usa)
+-- id INT unique, auto generated, can’t be null and is a primary key
+-- name VARCHAR(256) can’t be null
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa
+CREATE TABLE IF NOT EXISTS states(
+	id INT UNIQUE NOT NULL AUTO_INCREMENT,
+	NAME VARCHAR(256) NOT NULL,
+	PRIMARY KEY(id));
