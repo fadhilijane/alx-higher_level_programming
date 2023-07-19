f0a8a2dd6e364ca871179aa9e0ca7a6449064833
Jane Fadhili Njaga <janefadhili@gmail.com>
2023-07-19 08:06:09 -0700

    query

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
 -- create user_0d_1
-
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- give all the privileges
 GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
