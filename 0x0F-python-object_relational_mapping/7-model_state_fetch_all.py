2d435b8d34eca9e47fbf8ee3fd64049de1d2d24d
Jane Fadhili Njaga <janefadhili@gmail.com>
2023-08-20 00:50:25 -0700

    7-model_state_fetch_all

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -15,5 +15,5 @@ if __name__ == '__main__':
 
     session = Session(engine)
     for state in session.query(State).order_by(State.id).all():
-        print("{}:{}".format(state.id, state.name))
+        print("{}: {}".format(state.id, state.name))
     session.close()
