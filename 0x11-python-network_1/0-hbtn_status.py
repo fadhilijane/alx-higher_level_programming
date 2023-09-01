f1791635effeb1cc500446f47306b99579254916
fadhilijane <janefadhili@gmail.com>
2023-09-01 12:41:34 -0700

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" script that fetches https://alx-intranet.hbtn.io/status"""
+
+import urllib.request
+
+
+def fetchUrl():
+    """fetches url"""
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode("utf-8"))
+
+if __name__ == "__main__":
+    fetchUrl()
