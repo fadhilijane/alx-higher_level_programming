2e3073bc4ead5d56dda19e347f2230403d9368d2
Jane Fadhili Njaga <111033901+fadhilijane@users.noreply.github.com>
2024-05-30 09:21:15 +0300

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,17 +6,17 @@ function that finds peak in alist of integers
 
 def find_peak(list_of_integers):
     """
-    looks for peak
+    looks for peak in unsorted list
     """
     if not list_of_integers:
         return None
 
-    list_left, list_right = 0, len(list_of_integers) - 1
-    while list_left < list_right:
-        middle = (list_left + list_right) // 2
+    left, right = 0, len(list_of_integers) - 1
+    while left < right:
+        middle = (left + right) // 2
 
         if list_of_integers[middle] > list_of_integers[middle + 1}:
-            list_right = middle
+            right = middle
         else:
-            list_left = middle + 1
-    return list_of_integers[list_left]
+            left = middle + 1
+    return list_of_integers[left]
